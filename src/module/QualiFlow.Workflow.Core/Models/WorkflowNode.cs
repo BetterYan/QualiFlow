e1bb28cce251797e1a085ad932e06dcf42a60400
Zhuang Yan <wren_zhuang@qq.com>
2024-09-16 18:47:21 +0800

    add workflow project

---
+++ src/module/QualiFlow.Workflow.Core/Models/WorkflowNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualiFlow.Workflow.Core.Models;
+
+public class WorkflowNode
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public WorkflowNode Parent { get; set; }
+    public List<WorkflowNode> ChildrenNodes { get; set; }
+    public WorkflowNode()
+    {
+        ChildrenNodes = new List<WorkflowNode>();
+    }
+}
